90703abdda7f68f39e941542b6e203e336338cf0
cathe <pearsandcabbage@gmail.com>
2026-05-10 10:53:15 +0100

    scripts

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -24,6 +24,8 @@ in
   home.username = thenixuser.username;
   home.homeDirectory = thenixuser.home;
 
+  home.sessionPath = [ "$HOME/dots/bin" ];
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
