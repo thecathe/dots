617d7cac7aa73569872df9e864ce06360ccbcbd2
cathe <pearsandcabbage@gmail.com>
2026-05-25 13:29:13 +0100

    firefox user profile

--- modules/firefox/profiles.nix
+++ modules/firefox/profiles.nix
@@ -1,9 +1,18 @@
 { pkgs, ... }:
 
-let
-  theuser = import ./user.nix { inherit pkgs; };
-in
 {
   # https://mynixos.com/home-manager/options/programs.firefox.profiles.%3Cname%3E
-  ${theuser.name} = theuser."${theuser.name}";
+  cathe = {
+    id = 0;
+    name = "cathe";
+    isDefault = true;
+    settings = {
+      # "extensions.autoDisableScopes" = 0;
+    };
+    # extensions = {
+    #   packages = with pkgs.nur.repos.rycee.firefox-addons; [
+    #     ublock-origin
+    #   ];
+    # };
+  };
 }
