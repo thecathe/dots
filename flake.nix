1abf71ab4d08935d3e6ae250184a66985b8c99a1
cathe <pearsandcabbage@gmail.com>
2026-05-25 13:45:57 +0100

    flake.nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/master";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
