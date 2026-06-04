0414c61660c2b4188a646179d4ea600dbe8677a7
thecathe <pearsandcabbage@gmail.com>
2026-06-04 17:05:05 +0100

    nvim packages

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -28,7 +28,42 @@
     plugins = with pkgs.vimPlugins; [
 
       # ── Dependencies ────────────────────────────────────────────────────
-      plenary-nvim # lua library
+      # plenary-nvim # lua library
+
+      # ── Snacks (replaces telescope; add before other plugins) ────────────
+      {
+        plugin = snacks-nvim;
+        type = "lua";
+        config = ''
+          require('snacks').setup({
+            picker    = { enabled = true },  -- replaces telescope
+            explorer  = { enabled = true },  -- sidebar file tree
+            terminal  = { enabled = true },  -- toggleable terminal
+            notifier  = { enabled = true },  -- replaces vim.notify
+            indent    = { enabled = true },  -- indent guides
+            words     = { enabled = true },  -- highlight word under cursor
+            quickfile = { enabled = true },  -- faster file loading
+            bigfile   = { enabled = true },  -- disable heavy features on large files
+          })
+
+          -- Picker (replaces the telescope keymaps you had)
+          vim.keymap.set('n', '<leader>ff', function() Snacks.picker.files() end,            { desc = 'Find files' })
+          vim.keymap.set('n', '<leader>fg', function() Snacks.picker.grep() end,             { desc = 'Grep' })
+          vim.keymap.set('n', '<leader>fb', function() Snacks.picker.buffers() end,          { desc = 'Buffers' })
+          vim.keymap.set('n', '<leader>fd', function() Snacks.picker.diagnostics() end,      { desc = 'Diagnostics' })
+          vim.keymap.set('n', '<leader>fr', function() Snacks.picker.lsp_references() end,   { desc = 'LSP references' })
+          vim.keymap.set('n', '<leader>fs', function() Snacks.picker.lsp_symbols() end,      { desc = 'LSP symbols' })
+
+          -- Explorer
+          vim.keymap.set('n', '<leader>e', function() Snacks.explorer() end, { desc = 'File explorer' })
+
+          -- Terminal toggle
+          vim.keymap.set('n', '<leader>t',   function() Snacks.terminal() end, { desc = 'Toggle terminal' })
+          vim.keymap.set('t', '<Esc><Esc>',  '<C-\\><C-n>',                    { desc = 'Exit terminal mode' })
+        '';
+      }
+
+      which-key-nvim # show keymaps
       gitignore-nvim # git ignore
 
       # ── LSP ─────────────────────────────────────────────────────────────
@@ -124,20 +159,20 @@
       }
 
       # ── Fuzzy Finding ───────────────────────────────────────────────────
-      {
-        plugin = telescope-nvim;
-        type = "lua";
-        config = ''
-          local telescope = require('telescope')
-          local builtin   = require('telescope.builtin')
-          telescope.setup({})
-          vim.keymap.set('n', '<leader>ff', builtin.find_files,   { desc = 'Find files' })
-          vim.keymap.set('n', '<leader>fg', builtin.live_grep,    { desc = 'Live grep' })
-          vim.keymap.set('n', '<leader>fb', builtin.buffers,      { desc = 'Find buffers' })
-          vim.keymap.set('n', '<leader>fd', builtin.diagnostics,  { desc = 'Diagnostics' })
-          vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'LSP references' })
-        '';
-      }
+      # {
+      #   plugin = telescope-nvim;
+      #   type = "lua";
+      #   config = ''
+      #     local telescope = require('telescope')
+      #     local builtin   = require('telescope.builtin')
+      #     telescope.setup({})
+      #     vim.keymap.set('n', '<leader>ff', builtin.find_files,   { desc = 'Find files' })
+      #     vim.keymap.set('n', '<leader>fg', builtin.live_grep,    { desc = 'Live grep' })
+      #     vim.keymap.set('n', '<leader>fb', builtin.buffers,      { desc = 'Find buffers' })
+      #     vim.keymap.set('n', '<leader>fd', builtin.diagnostics,  { desc = 'Diagnostics' })
+      #     vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'LSP references' })
+      #   '';
+      # }
 
       # ── File Navigation ─────────────────────────────────────────────────
       {
