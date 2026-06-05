b7e565cb73e7c3efa6d5e0715b6103a5433035ec
thecathe <pearsandcabbage@gmail.com>
2026-06-05 13:42:19 +0100

    nvim erlfmt

--- modules/home/nvim/lang/erlang.lua
+++ modules/home/nvim/lang/erlang.lua
@@ -6,5 +6,5 @@ vim.lsp.enable('erlangls')
 
 -- erlang-ls does not support formatting; use erlfmt if available in PATH.
 -- Add it to your devShell and uncomment:
--- require('conform').formatters_by_ft.erlang = { 'erlfmt' }
+require('conform').formatters_by_ft.erlang = { 'erlfmt' }
 
