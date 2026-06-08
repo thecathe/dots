1077f31ad6f8c557c35fe12536a98963e0b68737
thecathe <pearsandcabbage@gmail.com>
2026-06-08 11:36:27 +0100

    kitty conf

--- modules/home/shell/kitty.nix
+++ modules/home/shell/kitty.nix
@@ -1,15 +1,30 @@
-{ pkgs, lib, ... }:
+{ lib, ... }:
 
 {
   programs.kitty = lib.mkForce {
     enable = true;
     shellIntegration.enableZshIntegration = true;
     settings = {
+      ##
       confirm_os_window_close = 0;
-      enable_audio_bell = false;
+      # allow_remote_control = "yes";
+      ##
       dynamic_background_opacity = true;
       background_opacity = "0.8";
       background_blur = 50;
+      ## 
+      enable_audio_bell = false;
+      ##
+      window_padding_width = 5;
+      window_border_width = "0.2pt";
+      ##
+      tab_bar_style = "powerline";
+      tab_powerline_style = "angled";
+      ##
+      cursor_trail = 10;
+      cursor_trail_decay = "0.1 0.4";
+      cursor_blink_interval = "1.0";
+      cursor_stop_blinking_after = "0";
     };
   };
 }
