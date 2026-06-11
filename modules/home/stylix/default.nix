da1cf10229341a65fa25e1fbd56e5e078a7b787d
thecathe <pearsandcabbage@gmail.com>
2026-06-11 19:38:14 +0100

    stylix remove fonts

--- modules/home/stylix/default.nix
+++ modules/home/stylix/default.nix
@@ -41,14 +41,14 @@
 #        package = pkgs.nerd-fonts.fira-code;
 #        name = "FiraCode Nerd Font Mono";
       };
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans";
-    };
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
-    };
+#    sansSerif = {
+#      package = pkgs.dejavu_fonts;
+#      name = "DejaVu Sans";
+#    };
+#    serif = {
+#      package = pkgs.dejavu_fonts;
+#      name = "DejaVu Serif";
+#    };
     };
   };
 }
