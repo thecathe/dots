75aa8e051618fe3c0e0f3e42df38339791717e4e
thecathe <pearsandcabbage@gmail.com>
2026-06-11 19:30:27 +0100

    stylix base16

--- modules/home/stylix/default.nix
+++ modules/home/stylix/default.nix
@@ -13,26 +13,26 @@
     };
     
 
-    ## base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 
-    base16Scheme = {
-      base00 = "282828"; # bg ----
-      base01 = "3c3836"; # bg ---
-      base02 = "504945"; # bg --
-      base03 = "665c54"; # bg -
-      base04 = "bdae93"; # fg +
-      base05 = "d5c4a1"; # fg ++
-      base06 = "ebdbb2"; # fg +++
-      base07 = "fbf1c7"; # fg ++++
-      base08 = "cc241d"; # red
-      base09 = "d65d0e"; # orange
-      base0A = "d79921"; # yellow
-      base0B = "98971a"; # green
-      base0C = "689d6a"; # aqua
-      base0D = "458588"; # blue
-      base0E = "b16286"; # purple
-      base0F = "d65d0e"; # brown
-    };
+    #base16Scheme = {
+    #  base00 = "282828"; # bg ----
+    #  base01 = "3c3836"; # bg ---
+    #  base02 = "504945"; # bg --
+    #  base03 = "665c54"; # bg -
+    #  base04 = "bdae93"; # fg +
+    #  base05 = "d5c4a1"; # fg ++
+    #  base06 = "ebdbb2"; # fg +++
+    #  base07 = "fbf1c7"; # fg ++++
+    #  base08 = "cc241d"; # red
+    #  base09 = "d65d0e"; # orange
+    #  base0A = "d79921"; # yellow
+    #  base0B = "98971a"; # green
+    #  base0C = "689d6a"; # aqua
+    #  base0D = "458588"; # blue
+    #  base0E = "b16286"; # purple
+    #  base0F = "d65d0e"; # brown
+    #};
 
     fonts = {
       monospace = {
