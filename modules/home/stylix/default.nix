503d4d99127e2fb02b62e9616f444a8c1313b63f
thecathe <pearsandcabbage@gmail.com>
2026-06-11 19:25:58 +0100

    stylix base16

--- modules/home/stylix/default.nix
+++ modules/home/stylix/default.nix
@@ -16,22 +16,22 @@
     ## base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 
     base16Scheme = {
-      base00 = "282828";
-      base01 = "3c3836";
-      base02 = "504945";
-      base03 = "665c54";
-      base04 = "bdae93";
-      base05 = "d5c4a1";
-      base06 = "ebdbb2";
-      base07 = "fbf1c7";
-      base08 = "fb4934";
-      base09 = "fe8019";
-      base0A = "fabd2f";
-      base0B = "b8bb26";
-      base0C = "8ec07c";
-      base0D = "83a598";
-      base0E = "d3869b";
-      base0F = "d65d0e";
+      base00 = "282828"; # bg ----
+      base01 = "3c3836"; # bg ---
+      base02 = "504945"; # bg --
+      base03 = "665c54"; # bg -
+      base04 = "bdae93"; # fg +
+      base05 = "d5c4a1"; # fg ++
+      base06 = "ebdbb2"; # fg +++
+      base07 = "fbf1c7"; # fg ++++
+      base08 = "cc241d"; # red
+      base09 = "d65d0e"; # orange
+      base0A = "d79921"; # yellow
+      base0B = "98971a"; # green
+      base0C = "689d6a"; # aqua
+      base0D = "458588"; # blue
+      base0E = "b16286"; # purple
+      base0F = "d65d0e"; # brown
     };
 
     fonts = {
