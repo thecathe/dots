e97ccbdc9ba49c7d505f4567d8113dba084bc030
thecathe <pearsandcabbage@gmail.com>
2026-06-11 19:21:12 +0100

    stylix base16

--- modules/home/stylix/default.nix
+++ modules/home/stylix/default.nix
@@ -11,7 +11,29 @@
       qt.enable = false; # disable for gnome
       ## gnome-shell.enable = false;
     };
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    
+
+    ## base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+
+    base16Scheme = {
+      base00 = "282828";
+      base01 = "3c3836";
+      base02 = "504945";
+      base03 = "665c54";
+      base04 = "bdae93";
+      base05 = "d5c4a1";
+      base06 = "ebdbb2";
+      base07 = "fbf1c7";
+      base08 = "fb4934";
+      base09 = "fe8019";
+      base0A = "fabd2f";
+      base0B = "b8bb26";
+      base0C = "8ec07c";
+      base0D = "83a598";
+      base0E = "d3869b";
+      base0F = "d65d0e";
+    };
+
     fonts = {
       monospace = {
         package = pkgs.nerd-fonts.jetbrains-mono;
