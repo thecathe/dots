c38f7349fc51a8e7408b8b33b18ff47aca2da091
thecathe <pearsandcabbage@gmail.com>
2026-06-11 14:03:00 +0100

    nvim blink-cmp

---
+++ modules/home/nvim/lua/config/blink-cmp.lua
@@ -0,0 +1,18 @@
+ require('blink.cmp').setup({
+      keymap = {
+        preset = 'default',
+        ['<Tab>']   = { 'select_next', 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+        ['<CR>']    = { 'accept', 'fallback' },
+        ['<C-e>']   = { 'hide' },
+        ['<C-b>']   = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>']   = { 'scroll_documentation_down', 'fallback' },
+      },
+      appearance = {
+        nerd_font_variant = 'mono',
+      },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+      signature = { enabled = true },  -- replaces the <C-k> signature help keymap
+    })
