5aaa75002afe31799d60518f006c4a59e9b015de
thecathe <pearsandcabbage@gmail.com>
2026-06-11 14:16:53 +0100

    stylix

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -22,10 +22,10 @@
   
   stylix = {
     enable = true;
-    targets = {
-      kitty.enable = true;
-      neovim.enable = true;
-    };
+    #targets = {
+    #  kitty.enable = true;
+    #  neovim.enable = true;
+    #};
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
   };
 
