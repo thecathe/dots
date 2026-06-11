8c38c2d737add86ae2b5eda995a24bf60d4b3d46
thecathe <pearsandcabbage@gmail.com>
2026-06-11 14:14:59 +0100

    stylix

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -19,6 +19,16 @@
     ../../modules/nixos
   ];
 
+  
+  stylix = {
+    enable = true;
+    targets = {
+      kitty.enable = true;
+      neovim.enable = true;
+    };
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+  };
+
   nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
 
   users.users.cathe = {
