9b08216d4e814f0d4e0a1e7d07f9f5df106200af
thecathe <pearsandcabbage@gmail.com>
2026-06-11 10:34:20 +0100

    language templates

---
+++ templates/erlang/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Erlang project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+         erlang
+          rebar3
+          erlang-language-platform
+          erlfmt        ];
+      };
+    };
+}
---
+++ templates/go/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Go project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go gopls gotools
+          goimports godoc
+          # delve
+          pkg-config
+        ];
+      };
+    };
+}
