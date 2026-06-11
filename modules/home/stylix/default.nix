5277c5c9af48dc1b7d039d13bd80a00829360a0c
thecathe <pearsandcabbage@gmail.com>
2026-06-11 18:33:04 +0100

    stylix diable for gnome

--- modules/home/stylix/default.nix
+++ modules/home/stylix/default.nix
@@ -7,6 +7,7 @@
       firefox.profileNames = [ "cathe" ];
       kitty.enable = true;
       neovim.enable = true;
+      qt.enable = false; # disable for gnome
     };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
   };
