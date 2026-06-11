6fb16809dc68084af080b162d22a94f3e86531b0
thecathe <pearsandcabbage@gmail.com>
2026-06-11 18:59:44 +0100

    stylix starship

--- modules/home/shell/starship.nix
+++ modules/home/shell/starship.nix
@@ -5,7 +5,7 @@
     enableZshIntegration = true;
     presets = [
       "nerd-font-symbols"
-#      "gruvbox-rainbow"
+      "gruvbox-rainbow"
       # "catppuccin-powerline"
     ];
     settings = {
@@ -20,43 +20,6 @@
           "Music" = "MUSI";
           "Pictures" = "PICS";
         };
-      };
-
-      nix_shell = {
-        format = "[$symbol$state( ($name))]($style) ";
-        disabled = false;
-        impure_msg = "[impure](bold red)";
-        pure_msg = "[pure](bold green)";
-        style = "bold blue";
-        symbol = " ";
-      };
-
-      ocaml = {
-        format = "[$symbol($version )(($switch_indicator$switch_name) )]($style)";
-        global_switch_indicator = "";
-        local_switch_indicator = "*";
-        style = "bold yellow";
-        symbol = "🐫 ";
-        version_format = "v$raw";
-        disabled = false;
-        detect_extensions = [
-          "opam"
-          "ml"
-          "mli"
-          "re"
-          "rei"
-        ];
-        detect_files = [
-          "dune"
-          "dune-project"
-          "jbuild"
-          "jbuild-ignore"
-          ".merlin"
-        ];
-        detect_folders = [
-          "_opam"
-          "esy.lock"
-        ];
       };
     };
   };
