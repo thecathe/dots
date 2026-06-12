e8205c854fb8a81879b84e8d4cb4f44d8660210b
thecathe <pearsandcabbage@gmail.com>
2026-06-12 16:17:26 +0100

    nvim bufferline

--- modules/home/nvim/lua/config/bufferline.lua
+++ modules/home/nvim/lua/config/bufferline.lua
@@ -5,13 +5,31 @@ require("bufferline").setup({
 		always_show_bufferline = true,
 		show_close_icon = false,
 		persist_buffer_sort = true,
-		close_command = "bdelete! %d",
+		close_command = function(bufnr)
+			Snacks.bufdelete(bufnr)
+		end,
+		right_mouse_command = function(bufnr)
+			Snacks.bufdelete(bufnr)
+		end,
+		offsets = {
+			{
+				filetype = "snacks_explorer",
+				text = "explorer",
+				separator = true,
+				text_align = "center",
+			},
+		},
 	},
 })
 
 -- Navigate between buffers (tabs)
 vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+
+-- close buffer
+vim.keymap.set("n", "<leader>bd", function()
+	Snacks.bufdelete()
+end, { desc = "Close buffer" })
 
 -- Reorder
 vim.keymap.set("n", "<leader>b>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer right" })
