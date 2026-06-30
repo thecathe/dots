a9044705e4d35818bd8d268cd7b3539132c56408
thecathe <pearsandcabbage@gmail.com>
2026-06-30 17:45:50 +0100

    stop waydroid autostart

--- modules/nixos/waydroid/default.nix
+++ modules/nixos/waydroid/default.nix
@@ -1,7 +1,15 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   virtualisation.waydroid = {
     enable = true;
     package = pkgs.waydroid-nftables;
   };
   environment.systemPackages = with pkgs; [wl-clipboard];
+  systemd.services.waydroid-container = {
+    wantedBy = lib.mkForce [];
+    serviceConfig.ExecStopPost = lib.mkForce "";
+  };
 }
