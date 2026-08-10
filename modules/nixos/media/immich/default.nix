33721eaff85bd1f26e6bcafe626702c6f76e0e00
thecathe <pearsandcabbage@gmail.com>
2026-08-10 16:03:31 +0100

    config immich

--- modules/nixos/media/immich/default.nix
+++ modules/nixos/media/immich/default.nix
@@ -6,4 +6,16 @@
     immich-cli ## cli
     ##    immich-go ## useful for go-import
   ];
+
+  services.immich = {
+    enable = true;
+    port = 2283;
+    host = "0.0.0.0";
+    openFirewall = true;
+    environment.IMMICH_LOG_LEVEL = "warn";
+    mediaLocation = "/home/cathe/Pictures/immich";
+    accelerationDevices = null;
+  };
+
+  users.users.immich.extraGroups = ["video" "render"];
 }
