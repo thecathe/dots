5bcb841a58a3ff08f79ae3aea7feaf86d7df001a
thecathe <pearsandcabbage@gmail.com>
2026-08-15 16:56:51 +0100

    fixed immich setup

--- modules/nixos/media/immich/default.nix
+++ modules/nixos/media/immich/default.nix
@@ -7,15 +7,19 @@
     ##    immich-go ## useful for go-import
   ];
 
+  # starts on: http://localhost:2283/
   services.immich = {
     enable = true;
     port = 2283;
     host = "0.0.0.0";
     openFirewall = true;
     environment.IMMICH_LOG_LEVEL = "warn";
-    mediaLocation = "/home/cathe/Pictures/immich";
+    mediaLocation = "/mnt/data/Multimedia/immich";
     accelerationDevices = null;
   };
+  systemd.tmpfiles.rules = [
+    "d /mnt/data/Multimedia/immich 0750 immich immich -"
+  ];
 
   users.users.immich.extraGroups = ["video" "render"];
 }
