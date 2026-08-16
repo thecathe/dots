aeda61830fee3879fcbcfceadd6cf3b612e2b4a3
thecathe <pearsandcabbage@gmail.com>
2026-08-16 11:29:45 +0100

    moved nixos specific modules

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -11,7 +11,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    ./modules
+    ./modules/nixos
     ./modules/wm/gnome
     ../../modules/nixos
   ];
