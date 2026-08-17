5350ab3ddb5a9090388ec96b5278c0c3822809db
Jonah Pears <pearsandcabbage@gmail.com>
2026-08-17 10:53:18 +0100

    change: shift-select/ctrl+shift-select and navigation

--- modules/home/shell/zsh/default.nix
+++ modules/home/shell/zsh/default.nix
@@ -1,4 +1,8 @@
-{...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [./aliases.nix];
   programs.bash.sessionVariables = {KP_EDITOR = "neovide";};
   programs.zsh = {
@@ -24,16 +28,37 @@
     ];
 
     sessionVariables = {KP_EDITOR = "neovide";};
-    initContent = ''
-      export PATH="$HOME/dots/bin:$PATH"
-      ##
-      webm2mp4() {
-        local input output
-        input="$(realpath "$1")"
-        output="$(realpath -, "$2")"
-        nix-shell -p ffmpeg --run \
-          "ffmpeg -i '$input' -c:v libx264 -crf 18 -c:a flac '$output'"
+    initContent = lib.mkMerge [
+      (lib.mkOrder 500 ''
+        export PATH="$HOME/dots/bin:$PATH"
+      '')
+      (lib.mkOrder 1000 ''
+        # forward/backward word
+        bindkey '^[[1;5C' forward-word
+        bindkey '^[[1;5D' backward-word
+      '')
+      (lib.mkOrder 1200 ''
+        webm2mp4() {
+          local input output
+          input="$(realpath "$1")"
+          output="$(realpath -, "$2")"
+          nix-shell -p ffmpeg --run \
+            "ffmpeg -i '$input' -c:v libx264 -crf 18 -c:a flac '$output'"
+        }
+      '')
+    ];
+
+    plugins = [
+      {
+        # shift-select/ctrl+shift-select
+        name = "zsh-shift-select";
+        src = pkgs.fetchFromGitHub {
+          owner = "jirutka";
+          repo = "zsh-shift-select";
+          rev = "47296f18c52e9cdff5ddf0c28a5cc8c88ef8696e";
+          hash = "sha256-4kUUBH2GTMb/d6PUNiSNFogkvDUSwMX823j4xsroJKs=";
+        };
       }
-    '';
+    ];
   };
 }
