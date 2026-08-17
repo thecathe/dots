e34105c21b4c1b9bb0448d5636106065768eae91
Jonah Pears <pearsandcabbage@gmail.com>
2026-08-17 10:57:02 +0100

    fix: stop kitty intercepting ctrl+shift+arrow keybinds

--- modules/home/shell/kitty.nix
+++ modules/home/shell/kitty.nix
@@ -9,6 +9,16 @@
         launch --type=overlay --title='nvim dots' zsh -c 'tmux attach -t dots:0.0 2>/dev/null || tmux new-session -s dots -c ~/dots nvim'
       '';
       # launch --type=overlay --title='nvim dots' zsh -c 'tmux attach -t dots:0.0 2>/dev/null || (tmux new-session -d -s dots -c ~/dots nvim && tmux split-window -v -t dots:0 -c ~/dots && tmux resize-pane -t dots:0.1 -y 2 && tmux attach -t dots)'
+
+      # free up so these can be used for select
+      # just use ctrl+tab/ctrl+shift+tab
+      "ctrl+shift+left" = "no_op";
+      "ctrl+shift+right" = "no_op";
+      # stop kitty intercepting these
+      "ctrl+shift+up" = "no_op";
+      "ctrl+shift+down" = "no_op";
+      "ctrl+shift+home" = "no_op";
+      "ctrl+shift+end" = "no_op";
     };
     settings = {
       ##
