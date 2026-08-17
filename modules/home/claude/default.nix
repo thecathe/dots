5fe586553146dc78761d00562916bca04faa5dc0
Jonah Pears <pearsandcabbage@gmail.com>
2026-08-17 15:06:53 +0100

    config: claude-code + vscode extension
    set default mode: Plan

--- modules/home/claude/default.nix
+++ modules/home/claude/default.nix
@@ -1,10 +1,14 @@
-{ lib, ... }: {
+{lib, ...}: {
   programs.claude-code = {
     enable = true;
     settings = {
       includeCoAuthoredBy = true; # # why lie?
       effortLevel = "xhigh";
       model = "opus";
+      permissions = {
+        defaultMode = "plan";
+        disableBypassPermissionsMode = "disable";
+      };
     };
   };
 }
--- modules/home/vscode/features/claude.nix
+++ modules/home/vscode/features/claude.nix
@@ -1,9 +1,8 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   extensions = with pkgs.vscode-extensions; [
     anthropic.claude-code
   ];
   settings = {
-
+    "claudeCode.initialPermissionMode" = "plan";
   };
 }
