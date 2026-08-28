e8e39e3868c4e7553f516a4c0264d52e3998b99b
thecathe <pearsandcabbage@gmail.com>
2026-08-28 11:33:29 +0100

    MAYBE fix: vscode latex recipe
    looks like i may have copied it over wrong, meaning that the "latex.tools" was inside of "view" rather than directly beneath "latex-workshop" -- NOTE: this still doesn't line up with what a "fresh" settings for the extension looks like, so i have no idea if this will work when i eventually get back into writing with latex again

--- modules/home/vscode/features/latex.nix
+++ modules/home/vscode/features/latex.nix
@@ -1,16 +1,16 @@
-{ pkgs, ... }:
-{
-  extensions = [
-    pkgs.vscode-extensions.james-yu.latex-workshop
-  ]
-  ++ [ pkgs.nix-vscode-extensions.vscode-marketplace-release-universal.phr0s.bib ];
+{pkgs, ...}: {
+  extensions =
+    [
+      pkgs.vscode-extensions.james-yu.latex-workshop
+    ]
+    ++ [pkgs.nix-vscode-extensions.vscode-marketplace-release-universal.phr0s.bib];
   settings = {
     "[latex]" = {
       "editor" = {
         "foldingStrategy" = "indentation";
         "wordWrap" = "wordWrapColumn";
         "wordWrapColumn" = 72;
-        "rulers" = [ 72 ];
+        "rulers" = [72];
         "defaultFormatter" = "James-Yu.latex-workshop";
       };
     };
@@ -24,8 +24,8 @@
         "linting.chktex.convertOutput.column.enabled" = false;
         "latex" = {
           "watch.pdf.delay" = 500;
-          "search.rootFiles.exclude" = [ "_*_only.tex" ];
-          "search.rootFiles.include" = [ "main.tex" ];
+          "search.rootFiles.exclude" = ["_*_only.tex"];
+          "search.rootFiles.include" = ["main.tex"];
         };
         "view" = {
           "outline.sections" = [
@@ -44,111 +44,111 @@
               "grayscale" = 0.2;
               "brightness" = 1.5;
             };
-
-            "latex.tools" = [
-              {
-                "name" = "latexmk";
-                "command" = "latexmk";
-                "args" = [
-                  # "-bibtex";
-                  "-shell-escape"
-                  "-synctex=1"
-                  "-interaction=nonstopmode"
-                  "-file-line-error"
-                  "-pdf"
-                  "-outdir=%OUTDIR%"
-                  "%DOC%"
-                ];
-                "env" = { };
-              }
-              {
-                "name" = "lualatexmk";
-                "command" = "latexmk";
-                "args" = [
-                  "-synctex=1"
-                  "-interaction=nonstopmode"
-                  "-file-line-error"
-                  "-lualatex"
-                  "-outdir=%OUTDIR%"
-                  "%DOC%"
-                ];
-                "env" = { };
-              }
-              {
-                "name" = "xelatexmk";
-                "command" = "latexmk";
-                "args" = [
-                  "-synctex=1"
-                  "-interaction=nonstopmode"
-                  "-file-line-error"
-                  "-xelatex"
-                  "-outdir=%OUTDIR%"
-                  "%DOC%"
-                ];
-                "env" = { };
-              }
-              {
-                "name" = "latexmk_rconly";
-                "command" = "latexmk";
-                "args" = [ "%DOC%" ];
-                "env" = { };
-              }
-              {
-                "name" = "pdflatex";
-                "command" = "pdflatex";
-                "args" = [
-                  "-synctex=1"
-                  "-interaction=nonstopmode"
-                  "-file-line-error"
-                  "%DOC%"
-                ];
-                "env" = { };
-              }
-              {
-                "name" = "bibtex";
-                "command" = "bibtex";
-                "args" = [ "%DOCFILE%" ];
-                "env" = { };
-              }
-              {
-                "name" = "rnw2tex";
-                "command" = "Rscript";
-                "args" = [
-                  "-e"
-                  "knitr::opts_knit$set(concordance = TRUE); knitr::knit('%DOCFILE_EXT%')"
-                ];
-                "env" = { };
-              }
-              {
-                "name" = "jnw2tex";
-                "command" = "julia";
-                "args" = [
-                  "-e"
-                  "using Weave; weave(\"%DOC_EXT%\"; doctype=\"tex\")"
-                ];
-                "env" = { };
-              }
-              {
-                "name" = "jnw2texmintex";
-                "command" = "julia";
-                "args" = [
-                  "-e"
-                  "using Weave; weave(\"%DOC_EXT%\"; doctype=\"texminted\")"
-                ];
-                "env" = { };
-              }
-              {
-                "name" = "tectonic";
-                "command" = "tectonic";
-                "args" = [
-                  "--synctex"
-                  "--keep-logs"
-                  "%DOC%.tex"
-                ];
-                "env" = { };
-              }
-            ];
           };
+
+          "latex.tools" = [
+            {
+              "name" = "latexmk";
+              "command" = "latexmk";
+              "args" = [
+                # "-bibtex";
+                "-shell-escape"
+                "-synctex=1"
+                "-interaction=nonstopmode"
+                "-file-line-error"
+                "-pdf"
+                "-outdir=%OUTDIR%"
+                "%DOC%"
+              ];
+              "env" = {};
+            }
+            {
+              "name" = "lualatexmk";
+              "command" = "latexmk";
+              "args" = [
+                "-synctex=1"
+                "-interaction=nonstopmode"
+                "-file-line-error"
+                "-lualatex"
+                "-outdir=%OUTDIR%"
+                "%DOC%"
+              ];
+              "env" = {};
+            }
+            {
+              "name" = "xelatexmk";
+              "command" = "latexmk";
+              "args" = [
+                "-synctex=1"
+                "-interaction=nonstopmode"
+                "-file-line-error"
+                "-xelatex"
+                "-outdir=%OUTDIR%"
+                "%DOC%"
+              ];
+              "env" = {};
+            }
+            {
+              "name" = "latexmk_rconly";
+              "command" = "latexmk";
+              "args" = ["%DOC%"];
+              "env" = {};
+            }
+            {
+              "name" = "pdflatex";
+              "command" = "pdflatex";
+              "args" = [
+                "-synctex=1"
+                "-interaction=nonstopmode"
+                "-file-line-error"
+                "%DOC%"
+              ];
+              "env" = {};
+            }
+            {
+              "name" = "bibtex";
+              "command" = "bibtex";
+              "args" = ["%DOCFILE%"];
+              "env" = {};
+            }
+            {
+              "name" = "rnw2tex";
+              "command" = "Rscript";
+              "args" = [
+                "-e"
+                "knitr::opts_knit$set(concordance = TRUE); knitr::knit('%DOCFILE_EXT%')"
+              ];
+              "env" = {};
+            }
+            {
+              "name" = "jnw2tex";
+              "command" = "julia";
+              "args" = [
+                "-e"
+                "using Weave; weave(\"%DOC_EXT%\"; doctype=\"tex\")"
+              ];
+              "env" = {};
+            }
+            {
+              "name" = "jnw2texmintex";
+              "command" = "julia";
+              "args" = [
+                "-e"
+                "using Weave; weave(\"%DOC_EXT%\"; doctype=\"texminted\")"
+              ];
+              "env" = {};
+            }
+            {
+              "name" = "tectonic";
+              "command" = "tectonic";
+              "args" = [
+                "--synctex"
+                "--keep-logs"
+                "%DOC%.tex"
+              ];
+              "env" = {};
+            }
+          ];
         };
       };
     };
