4617cba13fcbc127ebd69739c399780cf707196c
thecathe <pearsandcabbage@gmail.com>
2026-09-01 07:49:08 +0100

    fix: vscode settings
    - ignore recommendations
    - no auto update

--- modules/home/vscode/settings/global/extensions.nix
+++ modules/home/vscode/settings/global/extensions.nix
@@ -1,12 +1,12 @@
 {
   # When enabled, the notifications for extension recommendations will not be shown.
-  "extensions.ignoreRecommendations" = false;
+  "extensions.ignoreRecommendations" = true;
 
   # Controls the automatic update behavior of extensions. The updates are fetched from a Microsoft online service.
   #  - on: Download and install updates automatically only for enabled extensions.
   #  - off: Extensions are not automatically updated.
-  "extensions.autoUpdate" = "on";
+  "extensions.autoUpdate" = "off";
 
   # When enabled, automatically checks extensions for updates. If an extension has an update, it is marked as outdated in the Extensions view. The updates are fetched from a Microsoft online service.
-  "extensions.autoCheckUpdates" = true;
+  "extensions.autoCheckUpdates" = false;
 }
